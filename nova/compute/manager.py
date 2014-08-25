2d9a894d1486b53bc44e1c563139ab1d96ab70df
Vigneshvar.A.S <vigneshvar.a.s@gmail.com>
2014-08-25 19:43:55 +0530

    calling update_available_resource to update data before validation. Also needs to add validation if rpc object already present no need to create again

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -1206,6 +1206,7 @@ class ComputeManager(manager.Manager):
         filter - Ram, Cores and Disk
         """
 
+#	import pudb;pu.db
         # Check if sufficient RAM is available.
         requested_ram = instance_type.memory_mb
         free_ram_mb = host_state['free_ram_mb']
@@ -1293,7 +1294,7 @@ class ComputeManager(manager.Manager):
 #		LOG.info("Test variable %s",self.rpcserver_flavor)
 #	except:
 #		self.rpcserver_flavor={}
-
+	self.update_available_resource(context)
         for instance_type in instance_types:
                 # Replace '.' in the flavor name with '-' to avoid conflicts in the
                 # messaging layer
@@ -1313,6 +1314,8 @@ class ComputeManager(manager.Manager):
 			self.rpcserver_flavor[instance_type_topic]=rpc.get_server(target,endpoints, serializer)
 			self.rpcserver_flavor[instance_type_topic].start()
 		else:
+			LOG.info(_("Stopping and Deleting RPC server for %s")
+                            % instance_type_topic)
 			if (instance_type_topic in self.rpcserver_flavor.keys()):
 				self.rpcserver_flavor[instance_type_topic].stop()
 				del self.rpcserver_flavor[instance_type_topic]
