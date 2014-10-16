b9a9cb44a01aacf7dbd8e09ebf241c72cece74b9
Vigneshvar.A.S <viggates@gmail.com>
2014-10-16 20:03:27 +0000

    Added validation before calling "_subscribe_to_instance_type_topics" during create and terminate of instances.

--- nova/compute/manager.py
+++ nova/compute/manager.py
@@ -844,7 +844,8 @@ class ComputeManager(manager.Manager):
                         instance.uuid)
 	# I guess this is the place where subscription needs to refreshed.
         # Also need to validate if scalable scheduler is True
-        self._subscribe_to_instance_type_topics()
+	if CONF.bypass_scheduler:
+            self._subscribe_to_instance_type_topics()
 
     def _init_instance(self, context, instance):
         '''Initialize this instance during service init.'''
@@ -2307,7 +2308,8 @@ class ComputeManager(manager.Manager):
                 # *after* the resource tracker instance claim, as that is where
                 # the host is set on the instance.
 		#Assuming this is where instance is launched successfully. So we can resubscribe if required
-	        self._subscribe_to_instance_type_topics()
+		if CONF.bypass_scheduler:
+	            self._subscribe_to_instance_type_topics()
 
                 self._validate_instance_group_policy(context, instance,
                         filter_properties)
